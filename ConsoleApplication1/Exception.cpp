8183125657786e6f7d943b2a9fd5d7fc77fa63a7
arsen <arsen@DESKTOP-EIGMBHD>
2023-03-17 18:49:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Exception.cpp
@@ -0,0 +1,28 @@
+
+#include <SDKDDKVer.h>
+#include <stdio.h>
+#include <tchar.h>
+#include "Exception.h"
+
+
+Exception::Exception(char* message, int line)
+{
+	_message = message;
+	_line = line;
+}
+
+
+Exception::~Exception()
+{
+
+}
+
+char* Exception::GetMessage()
+{
+	return _message;
+}
+
+int Exception::GetLine()
+{
+	return _line;
+}
---
+++ ConsoleApplication1/SortingMethod.h
@@ -0,0 +1,6 @@
+
+enum SortingMethod
+{
+	BubbleSort,
+	QuickSort
+};
